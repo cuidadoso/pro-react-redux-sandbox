438cf4c25356a7d54295fd34ede11901ba1a4214
Alexander Pyreev <PyreevAS@msk.bcs.ru>
2019-03-01 14:47:09 +0300

    lesson 116 - mapDispatchToProps

--- src/components/counter.js
+++ src/components/counter.js
@@ -40,4 +40,4 @@ const mapDispatchToProps = (dispatch) => {
   }
 };
 
-export    default connect(mapStateToProps, mapDispatchToProps)(Counter);
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
