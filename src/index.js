0f5ba55e99977ad1fada6478bcc6c4cd4db283cc
Alexander Pyreev <PyreevAS@msk.bcs.ru>
2019-03-01 14:16:46 +0300

    lesson 113 - bindActionCreators

--- src/index.js
+++ src/index.js
@@ -1,23 +1,19 @@
-import { createStore } from "redux";
+import { createStore, bindActionCreators } from "redux";
 import { reducer } from "./reducer";
-import { dec, inc, rnd } from "./actions";
+import * as actions from "./actions";
 
 const store = createStore(reducer);
+const {dispatch} = store;
+const {inc, dec, rnd} = bindActionCreators(actions, dispatch);
 
-document.getElementById("inc").addEventListener("click",
-  () => {
-  store.dispatch(inc());
-});
+document.getElementById("inc").addEventListener("click", inc);
 
-document.getElementById("dec").addEventListener("click",
-  () => {
-  store.dispatch(dec());
-});
+document.getElementById("dec").addEventListener("click", dec);
 
 document.getElementById("rnd").addEventListener("click",
   () => {
   const payload = Math.floor(Math.random() * 10);
-  store.dispatch(rnd(payload));
+  rnd(payload);
 });
 
 const update = () => {
